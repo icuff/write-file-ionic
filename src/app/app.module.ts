e6a8a13a65e96b4c699f1dffeb2f98dc9754ffe0
bc-nogueira <bcnogueira@id.uff.br>
2019-05-30 21:46:10 -0300

    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { File } from '@ionic-native/file';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -22,6 +24,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    File,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { File } from '@ionic-native/file';
+
+declare var window;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public file: File) {}
 
+  writeToFile() {
+    var that = this;
+
+    window.resolveLocalFileSystemURL(this.file.externalRootDirectory, function (dirEntry) {
+      that.createFile(dirEntry, "fileToAppend.txt");
+    }, function(){ 
+      console.log('Ocorreu um erro') ;
+    });
   }
 
+  createFile(dirEntry, fileName) {
+    var that = this;
+
+    // Creates a new file or returns the file if it already exists.
+    dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+      that.writeFile(fileEntry);
+    }, function(){ 
+      console.log('Ocorreu um erro');
+    });
+  }
+
+  writeFile(fileEntry) {
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function (fileWriter) {
+      fileWriter.onwriteend = function() {
+        console.log("Successful file write...");
+        //readFile(fileEntry);
+      };
+      fileWriter.onerror = function (e) {
+          console.log("Failed file write: " + e.toString());
+      };
+
+      fileWriter.write('dataObj3');
+    });
+  }
 }
