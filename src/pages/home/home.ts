ebf81254daf172a57c7e9ebe5a8d2e20f5f1a4f8
bc-nogueira <bcnogueira@id.uff.br>
2019-06-11 23:34:15 -0300

    Add timer.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import moment from 'moment';
 
 import { File } from '@ionic-native/file';
 
@@ -11,30 +12,31 @@ declare var window;
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public file: File) {}
+  constructor(public navCtrl: NavController, public file: File, 
+    public toastController: ToastController) { }
 
   writeToFile() {
     var that = this;
-
-    window.resolveLocalFileSystemURL(this.file.externalRootDirectory, function (dirEntry) {
-      that.createFile(dirEntry, "fileToAppend.txt");
+    let beginTimer = moment().valueOf();
+    window.resolveLocalFileSystemURL(this.file.externalRootDirectory + 'Download/', function (dirEntry) {
+      that.createFile(dirEntry, "testIonic.txt", beginTimer);
     }, function(){ 
       console.log('Ocorreu um erro') ;
     });
   }
 
-  createFile(dirEntry, fileName) {
+  createFile(dirEntry, fileName, beginTimer) {
     var that = this;
 
     // Creates a new file or returns the file if it already exists.
     dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
-      that.writeFile(fileEntry);
+      that.writeFile(fileEntry, beginTimer);
     }, function(){ 
       console.log('Ocorreu um erro');
     });
   }
 
-  writeFile(fileEntry) {
+  writeFile(fileEntry, beginTimer) {
     // Create a FileWriter object for our FileEntry (log.txt).
     fileEntry.createWriter(function (fileWriter) {
       fileWriter.onwriteend = function() {
@@ -44,8 +46,17 @@ export class HomePage {
       fileWriter.onerror = function (e) {
           console.log("Failed file write: " + e.toString());
       };
-
-      fileWriter.write('dataObj3');
+      let content = '';
+      for(let i = 1; i <= 1000; i++) {
+        content = content + 'Line ' + i + '\n';
+      }
+      fileWriter.write(content);
     });
+    let endTimer = moment().valueOf();
+    const toast = this.toastController.create({
+      message: 'Finished in ' + (endTimer - beginTimer) + 'ms',
+      duration: 200000
+    });
+    toast.present();
   }
 }
